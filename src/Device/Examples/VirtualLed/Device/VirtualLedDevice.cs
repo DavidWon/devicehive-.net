bef1d23809d89232358f8bee0552fdfd6b78c080
jwyou <feelyoo75@gmail.com>
2015-04-14 17:13:51 +0900

    첫 번째 데모 코드

--- src/Device/Examples/VirtualLed/Device/VirtualLedDevice.cs
+++ src/Device/Examples/VirtualLed/Device/VirtualLedDevice.cs
@@ -8,12 +8,13 @@ namespace VirtualLed
     /// <summary>
     /// Represents VirtualLed device implementation
     /// </summary>
-    [Device("E50D6085-2ABA-48E9-B1C3-73C673E414BE", "05F94BF509C8", "Sample VirtualLed Device")]
-    [DeviceClass("Sample VirtualLed Device", "1.0", OfflineTimeout = 600)]
-    [DeviceEquipment(VirtualLedDevice.LED_CODE, "VirtualLED", "Controllable LED")]
+    /// [Device("E50D6085-2ABA-48E9-B1C3-73C673E414BE", "05F94BF509C8", "Sample VirtualLed Device")]
+    [Device("C8B09E30-CE49-11E4-8830-0800200C9A66", "05F94BF509C8", "동부화재 2층 프린터 1")]
+    [DeviceClass("아이블럭2 Sample", "1.0.1", OfflineTimeout = 600)]
+    [DeviceEquipment(VirtualLedDevice.INK_NOTI_CODE, "잉크알리미", "잉크 알림 가능")]
     public class VirtualLedDevice : DeviceBase
     {
-        private const string LED_CODE = "LED";   // LED equipment code
+        private const string INK_NOTI_CODE = "INK_NOTI";   // LED equipment code
         private int _ledState = 0;               // LED state
 
         #region DeviceBase Members
@@ -25,7 +26,7 @@ namespace VirtualLed
         public override void Main(CancellationToken token)
         {
             // send the notification about initial LED state
-            ServiceChannel.SendEquipmentNotification(LED_CODE, new LedNotification(_ledState));
+            ServiceChannel.SendEquipmentNotification(INK_NOTI_CODE, new LedNotification(_ledState));
             
             // do nothing in the main thread
         }
@@ -43,7 +44,7 @@ namespace VirtualLed
         public DeviceCommandResult UpdateLedState(UpdateLedStateCommand command, CancellationToken token)
         {
             // validate input parameters
-            if (command.Equipment != LED_CODE)
+            if (command.Equipment != INK_NOTI_CODE)
                 return new DeviceCommandResult("Failed", "Invalid equipment parameter: " + command.Equipment);
             if (command.State == null || !new[] { 0, 1 }.Contains(command.State.Value))
                 return new DeviceCommandResult("Failed", "Invalid state parameter: " + command.State);
@@ -57,6 +58,16 @@ namespace VirtualLed
 
             // return the Completed status
             return new DeviceCommandResult("Completed");
+        }
+
+        public void UpdateLedState(int state)
+        {
+            if (state == 9)
+            {
+                _ledState = 0;
+            }
+
+            ServiceChannel.SendEquipmentNotification(INK_NOTI_CODE, new LedNotification(state));
         }
         #endregion
     }
--- src/Device/Examples/VirtualLed/Program.cs
+++ src/Device/Examples/VirtualLed/Program.cs
@@ -14,10 +14,10 @@ namespace VirtualLed
 
                 // create a RestfulDeviceService used to communicate with the DeviceHive cloud
                 // insert your assigned DeviceHive service URL here
-                using (var service = new RestfulDeviceService("http://localhost/DeviceHive.API"))
+                using (var service = new RestfulDeviceService("http://192.168.1.112:8080/DeviceHiveJava/rest"))
                 {
                     // create a DeviceHive network where our device will reside
-                    var network = new Network("VirtualLed Sample Network", "A DeviceHive network for VirtualLed sample");
+                    var network = new Network("윤성환 iBlock2 담당 지역", "윤성환 관리자가 담당하고 있는 동대구 iblock 지역");
 
                     // create a DeviceHost used to run our device
                     var deviceHost = new DeviceHost(service, network);
@@ -30,15 +30,55 @@ namespace VirtualLed
                     deviceHost.Start();
 
                     // wait for console key press and then stop the host
-                    Console.WriteLine("Device is now running, press any key to stop...");
-                    Console.ReadKey();
+                    Console.WriteLine("잉크알리미의 상태를 변경하려면 숫자를 입력하세요.");
+                    Console.WriteLine("1 : 빨간색 잉크 부족");
+                    Console.WriteLine("2 : 노란색 잉크 부족");
+                    Console.WriteLine("3 : 파란색 잉크 부족");
+                    Console.WriteLine("4 : 검정색 잉크 부족");
+                    Console.WriteLine("잉크알리미를 종료하려면 q 를 눌러주세요.");
+
+                    ConsoleKeyInfo keyInfo;
+                    do
+                    {
+                        keyInfo = Console.ReadKey();
+                        int state = 0;
+                        switch (Char.ToLower(keyInfo.KeyChar))
+                        {
+                            case '1':
+                                Console.WriteLine("  빨간색 잉크가 부족합니다.");
+                                state = 1;
+                                break;
+                            case '2': 
+                                Console.WriteLine("  노란색 잉크가 부족합니다.");
+                                state = 2;
+                                break;
+                            case '3': 
+                                Console.WriteLine("  파란색 잉크가 부족합니다.");
+                                state = 4;
+                                break;
+                            case '4': 
+                                Console.WriteLine("  검정색 잉크가 부족합니다.");
+                                state = 8;
+                                break;
+                            case '5':
+                                Console.WriteLine("  잉크교체가 완료 되었습니다.");
+                                state = 9;
+                                break;
+                        }
+                        
+                        led.UpdateLedState(state);
+                        
+
+                    } while (Char.ToLower(keyInfo.KeyChar) != 'q');
+
+                    // Console.ReadKey();
                     deviceHost.Stop();
                 }
             }
             catch (Exception ex)
             {
                 // handle the error
-                Console.WriteLine("Error: " + ex);
+                Console.WriteLine("에러 Error: " + ex);
                 Console.ReadKey();
             }
         }
